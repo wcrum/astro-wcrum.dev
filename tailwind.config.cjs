c58f607b67c2780a10868cdfb7a41ec72d149917
will <will@wcrum.dev>
2026-01-26 12:08:58 -0700

    this is a test commit
    Signed-off-by: will <will@wcrum.dev>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,3 @@
-module.exports = {
-  content: ["./src/**/*.{astro,html,svelte,vue,cjs,js,ts,jsx,tsx}"],
-  plugins: [require("daisyui")],
-  theme: {
-    extend: {
-      backgroundImage: {
         'contours': "url('/contours.png')",
       }
     }
